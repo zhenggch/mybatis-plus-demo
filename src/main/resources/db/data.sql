86fd40d8bb615d4d68793418f065a64b62d3c7a6
udblfd <udblfd@163.com>
2019-05-30 13:48:49 +0800

    增加测试数据

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,8 +1,13 @@
 DELETE FROM user;
 
 INSERT INTO user (id, name, age, email) VALUES
-(1, 'Jone', 18, 'test1@baomidou.com'),
-(2, 'Jack', 20, 'test2@baomidou.com'),
-(3, 'Tom', 28, 'test3@baomidou.com'),
-(4, 'Sandy', 21, 'test4@baomidou.com'),
-(5, 'Billie', 24, 'test5@baomidou.com');
+(1, 'test1', 16, 'test1@163.com'),
+(2, 'test2', 16, 'test2@163.com'),
+(3, 'test3', 16, 'test3@163.com'),
+(4, 'test4', 20, 'test4@163.com'),
+(5, 'test5', 20, 'test5@163.com'),
+(6, 'test6', 20, 'test6@163.com'),
+(7, 'test7', 24, 'test7@163.com'),
+(8, 'test8', 24, 'test8@163.com'),
+(9, 'test9', 24, 'test9@163.com'),
+(10, 'test10', 24, 'test10@163.com');
