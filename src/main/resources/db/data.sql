dd24a46bdf7c299de3729f40c380ff59610eef88
udblfd <udblfd@163.com>
2019-06-03 11:42:34 +0800

    User 表增加“创建人”、“创建时间”、“修改人” 和 “修改时间”字段

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -1,13 +1,13 @@
 DELETE FROM user;
 
-INSERT INTO user (id, name, age, email, is_deleted) VALUES
-(1, 'test1', 16, 'test1@163.com', 1),
-(2, 'test2', 16, 'test2@163.com', 1),
-(3, 'test3', 16, 'test3@163.com', 0),
-(4, 'test4', 20, 'test4@163.com', 0),
-(5, 'test5', 20, 'test5@163.com', 0),
-(6, 'test6', 20, 'test6@163.com', 0),
-(7, 'test7', 24, 'test7@163.com', 0),
-(8, 'test8', 24, 'test8@163.com', 0),
-(9, 'test9', 24, 'test9@163.com', 0),
-(10, 'test10', 24, 'test10@163.com', 0);
+INSERT INTO user (id, name, age, email, is_deleted, create_id, create_time, update_id, update_time) VALUES
+(1, 'test1', 16, 'test1@163.com', 1, 1, '2019-06-01', 1, '2019-06-01'),
+(2, 'test2', 16, 'test2@163.com', 1, 1, '2019-06-01', 1, '2019-06-01'),
+(3, 'test3', 16, 'test3@163.com', 0, 1, '2019-06-01', 1, '2019-06-01'),
+(4, 'test4', 20, 'test4@163.com', 0, 1, '2019-06-01', 1, '2019-06-01'),
+(5, 'test5', 20, 'test5@163.com', 0, 1, '2019-06-01', 1, '2019-06-01'),
+(6, 'test6', 20, 'test6@163.com', 0, 1, '2019-06-01', 1, '2019-06-01'),
+(7, 'test7', 24, 'test7@163.com', 0, 1, '2019-06-01', 1, '2019-06-01'),
+(8, 'test8', 24, 'test8@163.com', 0, 1, '2019-06-01', 1, '2019-06-01'),
+(9, 'test9', 24, 'test9@163.com', 0, 1, '2019-06-01', 1, '2019-06-01'),
+(10, 'test10', 24, 'test10@163.com', 0, 1, '2019-06-01', 1, '2019-06-01');
