3c32e998fb7739483f37bb035020eca6d74d3aa8
udblfd <udblfd@163.com>
2019-06-03 11:43:44 +0800

    增加“创建人”、“创建时间”、“修改人” 和 “修改时间”字段的自动填充功能

--- src/main/java/demo/handler/MyMetaObjectHandler.java
+++ src/main/java/demo/handler/MyMetaObjectHandler.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 /**
  * Mybatis Plus 自动填充功能
  *
@@ -21,11 +23,16 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
     public void insertFill(MetaObject metaObject) {
         LOGGER.info("start insert fill ....");
         metaObject.setValue("deletedFlag","0");
+        metaObject.setValue("createId",1);
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("updateId",1);
+        metaObject.setValue("updateTime", LocalDateTime.now());
     }
 
     @Override
     public void updateFill(MetaObject metaObject) {
         LOGGER.info("start update fill ....");
-        metaObject.setValue("deletedFlag","0");
+        metaObject.setValue("updateId",9);
+        metaObject.setValue("updateTime", LocalDateTime.now());
     }
 }
