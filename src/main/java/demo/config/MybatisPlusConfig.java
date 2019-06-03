fa1024301c027186d0ffc3efbabac89e43ffbdde
udblfd <udblfd@163.com>
2019-06-03 11:37:47 +0800

    增加SQL执行效率插件

--- src/main/java/demo/config/MybatisPlusConfig.java
+++ src/main/java/demo/config/MybatisPlusConfig.java
@@ -1,6 +1,7 @@
 package demo.config;
 
 import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,5 +24,13 @@ public class MybatisPlusConfig {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
+    }
+
+    /**
+     * SQL执行效率插件
+     */
+    @Bean
+    public PerformanceInterceptor performanceInterceptor() {
+        return new PerformanceInterceptor();
     }
 }
